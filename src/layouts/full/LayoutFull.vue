19e1de729a284feb2472c0ea047afcb2db2e5660
Praveen Sundaresan <56668909+praveenisgreat@users.noreply.github.com>
2023-03-03 11:11:45 +0800

    Add files via upload

---
+++ src/layouts/full/LayoutFull.vue
@@ -0,0 +1,25 @@
+<template>
+  <layout-full>
+    <router-view />
+    <!-- Using `customizer` slot to render customizer -->
+    <app-customizer v-if="showCustomizer" slot="customizer" />
+  </layout-full>
+</template>
+
+<script>
+import LayoutFull from '@core/layouts/layout-full/LayoutFull.vue'
+import AppCustomizer from '@core/layouts/components/app-customizer/AppCustomizer.vue'
+import { $themeConfig } from '@themeConfig'
+
+export default {
+  components: {
+    LayoutFull,
+    AppCustomizer,
+  },
+  data() {
+    return {
+      showCustomizer: $themeConfig.layout.customizer,
+    }
+  },
+}
+</script>
