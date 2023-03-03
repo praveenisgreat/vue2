de523d03e35e9f2b4ad972c62512a0f2e41d6e17
Praveen Sundaresan <56668909+praveenisgreat@users.noreply.github.com>
2023-03-03 11:34:08 +0800

    Add files via upload

---
+++ src/views/SecondPage_old.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-row>
+    <b-col cols="12">
+      <collapse-types />
+    </b-col>
+    
+  </b-row>
+</template>
+
+<script>
+import { BRow, BCol } from 'bootstrap-vue'
+import CollapseTypes from './collapse/CollapseTypes.vue'
+
+export default {
+  components: {
+    BRow,
+    BCol,
+    CollapseTypes,
+  },
+}
+</script>
---
+++ src/views/pages/contents/CardRegistration.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-row>
+      <b-col cols="10">
+        <collapse-types />
+      </b-col>
+      <b-col cols="2">
+        <collapse-types />
+      </b-col>
+    </b-row>
+  </template>
+  
+  <script>
+  import { BRow, BCol } from 'bootstrap-vue'
+  import CollapseTypes from '@/views/collapse/CollapseTypes.vue'
+  
+  export default {
+    components: {
+      BRow,
+      BCol,
+      CollapseTypes,
+    },
+  }
+  </script>
+  
